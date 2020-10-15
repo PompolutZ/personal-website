81e693c3339645a25bddf6ef2cfc5c6ba4cc2e6a
Oleh Lutsenko <pompolutz@gmail.com>
2020-10-15 21:11:17 +0200

    set initial structure

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import Nav from '../components/nav'
+import Nav from "../components/nav";
 
 export default function IndexPage() {
   return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
-        </h1>
+    <div className="w-full h-full grid grid-rows-layout place-items-center">
+      <div className="py-20 grid">
+        <h1 className="text-5xl text-center text-accent-1">oleh lutsenko</h1>
       </div>
+      <Nav />
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,20 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true,
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'accent-1': '#333',
+        "accent-1": "#333",
+      },
+      gridTemplateRows: {
+        layout: "1fr auto",
       },
     },
   },
   variants: {},
   plugins: [],
-}
+};
