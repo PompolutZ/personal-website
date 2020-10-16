8cff4507a7491647056d04c5e8e9bee116434bb8
Oleh Lutsenko <pompolutz@gmail.com>
2020-10-16 18:15:58 +0200

    remove import font from css into a header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
+import Head from "next/head";
 import Nav from "../components/nav";
 
 export default function IndexPage() {
   return (
-    <div className="w-full h-full grid grid-rows-layout place-items-center">
-      <div className="py-20 grid">
-        <h1 className="text-5xl">oleh lutsenko</h1>
+    <>
+      <Head>
+        <title>Oleh Lutsenko - Developer</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital@1&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <div className="w-full h-full grid grid-rows-layout place-items-center">
+        <div className="py-20 grid">
+          <h1 className="text-5xl">oleh lutsenko</h1>
+        </div>
+        <Nav />
       </div>
-      <Nav />
-    </div>
+    </>
   );
 }
